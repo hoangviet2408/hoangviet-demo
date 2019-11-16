71d4bebfa90305bd00eb8709bc1df156d2149d75
Hoang Viet <vietht098@gmail.com>
2019-11-16 09:28:00 +0700

    Add

---
+++ demo3.js
@@ -0,0 +1,10 @@
+function Hello(hoten) {
+    console.log("Chao Ban " + hoten);
+}
+function Goodbye() {
+    return " tam biet";
+}
+Hello("Sunflower");
+var x = Goodbye();
+console.log(x);
+//# sourceMappingURL=demo3.js.map
